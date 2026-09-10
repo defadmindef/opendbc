53102c94561c14fb66328f9ad67134ba5a4d76f5
defadmindef <defadmindef@users.noreply.github.com>
2026-09-10 16:42:06 -0600

    Add 2026 Ioniq 5 support

--- opendbc_repo/opendbc/car/hyundai/values.py
+++ opendbc_repo/opendbc/car/hyundai/values.py
@@ -391,6 +391,13 @@ class CAR(Platforms):
     HYUNDAI_IONIQ_5.specs,
     flags=HyundaiFlags.EV | HyundaiFlags.CANFD_NO_RADAR_DISABLE,
   )
+  HYUNDAI_IONIQ_5_2026 = HyundaiCanFDPlatformConfig(
+    [
+      HyundaiCarDocs("Hyundai Ioniq 5 Limited AWD 2026", "Highway Driving Assist II", car_parts=CarParts.common([CarHarness.hyundai_k])),
+    ],
+    CarSpecs(mass=2180, wheelbase=3.00, steerRatio=14.26, tireStiffnessFactor=0.65),
+    flags=HyundaiFlags.EV,
+  )
   HYUNDAI_TUCSON_4TH_GEN = HyundaiCanFDPlatformConfig(
     [
       HyundaiCarDocs("Hyundai Tucson 2022", car_parts=CarParts.common([CarHarness.hyundai_n])),
